6eeefc2f59ad9edaf3643873c1d98a6788fb8c23
Claire Cashmore <clairecashmore2017@gmail.com>
2022-07-20 19:29:56 -0400

    Employee Test beginning

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,10 @@
 const Employee = require('../lib/Employee');
+
+//first test
+test('creates an employee object', () => {
+    const employee = new Employee('Claire', 1234, 'email@gmail.com');
+
+    expect(employee.name).toBe('Claire');
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toBe('email@gmail.com');
+})
