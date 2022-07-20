ae1e02fa13378e02db97d2c5706c3400516ad3ae
Claire Cashmore <clairecashmore2017@gmail.com>
2022-07-20 19:54:02 -0400

    succesful test for engineer object creation

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,10 @@
 const Engineer = require('../lib/Engineer');
+
+//test to create an engineer object
+test('creates an Engineer object', () => {
+    const engineer = new Engineer('Claire', 1, 'email');
+
+    expect(engineer.name).toBe('Claire');
+    expect(engineer.id).toEqual(expect.any(Number));
+    expect(engineer.email).toBe('email');
+})
---
+++ index.js
@@ -0,0 +1,4 @@
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+
+console.log(new Engineer('Claire', 1, 'Email').getRole());
