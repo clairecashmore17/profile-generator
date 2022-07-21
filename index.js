756a64c5a425a1784d1ae58fd0c35c9a4debb788
Claire Cashmore <clairecashmore2017@gmail.com>
2022-07-20 20:55:37 -0400

    prompts for team manager details

--- index.js
+++ index.js
@@ -1,4 +1,43 @@
 const Employee = require('./lib/Employee');
 const Engineer = require('./lib/Engineer');
+const inquirer = require('inquirer');
+const Manager = require('./lib/Manager');
 
-console.log(new Engineer('Claire', 1, 'Email').getRole());
+
+inquirer
+.prompt([   
+    {
+        type: 'text',
+        name: 'name',
+        message : "What is your team manager's name?"
+    },
+    {
+        type:'number',
+        name: 'id',
+        message: 'What is your their ID?'
+    },
+    {
+        type: 'text',
+        name: 'email',
+        message: 'What is their email?'
+    },
+    {
+        type: 'confirm',
+        name: 'addMember',
+        message: 'Would you like to add another team member?'
+    }
+    
+])
+.then(({ name,id,email, addMember}) => {
+    const manager = new Manager(name,id,email);
+    console.log(manager);
+    if(addMember){
+        console.log('You want to add another member!');
+        return;
+
+    } else {
+        console.log('No members to add');
+        return;
+    }
+    
+})
