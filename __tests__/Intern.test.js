61f9bb4505cddb7bb45b7b245443194e5492b3e0
Claire Cashmore <clairecashmore2017@gmail.com>
2022-07-20 20:31:31 -0400

    basic test for intern AND manager

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,10 @@
 const Intern = require('../lib/Intern');
+
+//create an intern object
+test('create an intern object', () => {
+    const intern = new Intern('Claire', 1234, 'email@gmail.com');
+
+    expect(intern.name).toBe('Claire');
+    expect(intern.id).toEqual(expect.any(Number));
+    expect(intern.email).toBe('email@gmail.com');
+});
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,10 @@
 const Manager = require('../lib/Manager');
+
+//create an Manager object
+test('create an Manager object', () => {
+    const manager = new Manager('Claire', 1234, 'email@gmail.com');
+
+    expect(manager.name).toBe('Claire');
+    expect(manager.id).toEqual(expect.any(Number));
+    expect(manager.email).toBe('email@gmail.com');
+});
